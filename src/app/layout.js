8952d17bf202ff2cde34f504d00413e6e3a854df
Divyank Shah <divyank.shah.2016@gmail.com>
2023-06-30 13:51:08 -0400

    formatting

--- src/app/layout.js
+++ src/app/layout.js
@@ -26,8 +26,8 @@ export default function RootLayout({ children }) {
       <body>
         <Navigation />
         {children}
-        <Footer/>
-        </body>
+        <Footer />
+      </body>
     </html>
   );
 }
