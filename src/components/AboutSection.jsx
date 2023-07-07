db5991836554c9bcd115f20f42fa63dab0613803
Menthy <wumengxuan0728@gmail.com>
2023-07-07 16:51:52 -0700

    fix

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -1,41 +1,43 @@
 import React from "react";
 import PADsvg from "./PADsvg.jsx";
 import Boxessvg from "./Boxessvg";
+import triangle from "../../public/triangle.svg";
+import Image from "next/image.js";
+import Link from "next/link.js";
 
 const AboutSection = () => {
   return (
-    <div className="my-24">
-      <div className="absolute scale-75  rotate-90 top-12 bottom-14 -left-16 ">
+    <div className="w-full relative flex font-playfair">
+      <div className="rotate-90 origin-bottom-left top-0 left-0 absolute -translate-y-full">
         <PADsvg color="#E7DBDF" />
       </div>
-
-      <div className="flex items-center justify-center -ml-16 mr-12 -m-10 mt-7  text-xl text-violet-200 ">
-        <p className="-ml-6">About Us</p>
-      </div>
-
-      <div className="flex items-center justify-center ml-14 -mr-4 mt-8  text-2xl font-bold">
-        <p>Welcome to Phi Alpha Delta</p>
-      </div>
-
-      <div className="flex items-center justify-center ml-14 -mr-12">
-        <p className="ml-6 -mr-20">
-          We are the preeminent law fraternity promoting the bonds of <br />
-          fraternalism and we are theleader in the devolopment
-          <br />
-          advancement of professional ideals.
-        </p>
-      </div>
-
-      <div className="w-full flex justify-center text-white ml-16 ">
-        <button className="bg-purple-700 ml-20 -mr-20 left-20 relative py-1.5 px-12 rounded">
-          Learn More
-        </button>
-      </div>
-      <div className="flex justify-center -ml-14 mr-8 -mt-14 ">
-        <div className="-left-20 relative h-[15vh] w-2 bg-[#C3ACD0] -mt-14 -top-14"></div>
-      </div>
-      <div className="absolute top-0 right-0">
+      <div className="absolute top-5 right-5">
         <Boxessvg color="#E7DBDF" />
+      </div>
+      <div className="ml-[30%] w-full mt-24">
+        <p className="text-4xl text-pad-lightpurple font-bold ml-8 ">
+          About Us
+        </p>
+        <div className="text-5xl font-bold border-l-8 border-pad-lightpurple p-3">
+          <p>Welcome to Phi Alpha Delta</p>
+          <p className="text-2xl w-2/3">
+            We are the preeminent law fraternity promoting the bonds of
+            fraternalism and we are theleader in the devolopment advancement of
+            professional ideals.
+          </p>
+        </div>
+
+        <div className="w-full flex justify-end">
+          <Link
+            className="bg-pad-purple h-12 flex items-center no-underline justify-start mr-48"
+            href="/about"
+          >
+            <Image src={triangle} className="h-full m-0" />
+            <p className="px-16 m-0 font-bold text-pad-beige text-3xl">
+              Learn More
+            </p>
+          </Link>
+        </div>
       </div>
     </div>
   );
