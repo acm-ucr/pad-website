96076a37102c86f72e81ebccd97a878fd2168fea
Menthy <wumengxuan0728@gmail.com>
2023-07-12 15:05:18 -0700

    fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,28 +6,28 @@ import { GrMail } from "react-icons/gr";
 
 const Footer = () => {
   return (
-    <div className="relative bottom-0 w-full h-full bg-[#745091]">
-      <div className="scale-50  w-1/3  absolute translate-y-3/4 right-5">
+    <div className="relative w-full bg-pad-purple h-[375px] overflow-hidden flex justify-between items-end">
+      <div className="absolute bottom-0 right-0 scale-50 translate-x-1/4 translate-y-1/4">
         <PADsvg color="#674188" />
       </div>
 
-      <div className="scale-50  w-1/3 absolute -translate-x-1/4 ">
+      <div className="w-1/3 absolute bottom-1">
         <Scalesvg color="#674188" />
       </div>
-
-      <div className=" flex flex-col justify-end relative font-teko text-end  text-white font-semibold md:text-1xl pr-10 mr-10 h-[375px] ">
-        <div className="">Contact us</div>
-        <div className="">900 University Ave,</div>
-        <div>Riverside, CA 92521</div>
-      </div>
-
-      <div className="flex flex-col relative items-end -translate-y-2/3 bottom-5 right-7">
-        <BsDiscord className="  md:text-4xl text-white font-semibold" />
-        <GrMail className=" md:text-4xl text-white font-semibold" />
-      </div>
-
-      <div className="flex flex-col -translate-y-2/3 relative left-6 h-[170px]">
+      <div className="flex flex-col relative">
         <div className="text-white font-semibold text-sm"> Phi Alpha Delta</div>
+      </div>
+      <div className="flex">
+        <div className="relative flex flex-col justify-end text-end border-r-2 border-white text-white md:text-1xl pr-10 mr-10">
+          <div className="">Contact us</div>
+          <div className="">900 University Ave,</div>
+          <div>Riverside, CA 92521</div>
+        </div>
+
+        <div className="flex flex-col relative items-end ">
+          <BsDiscord className="  md:text-4xl text-white font-semibold" />
+          <GrMail className=" md:text-4xl text-white font-semibold" />
+        </div>
       </div>
     </div>
   );
