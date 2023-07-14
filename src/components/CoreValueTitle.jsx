be33d55db1b1777694853f75734de1f168593da2
Menthy <wumengxuan0728@gmail.com>
2023-07-14 09:48:25 -0700

    fix core

--- src/components/CoreValueTitle.jsx
+++ src/components/CoreValueTitle.jsx
@@ -2,24 +2,23 @@ import React from "react";
 
 const CoreValueTitle = () => {
   return (
-    <div className="flex relative top-64 h-52">
-      <div className="flex flex-col h-24 w-80">
-        <div className="outline outline-pad-lightpuple absolute left-40 h-40 w-96">
-          <div className="bg-pad-purple absolute left-2 top-2 h-44 w-96">
-            <div className="flex flex-row-reverse h-32 w-full relative">
-              <div className="bg-pad-lightpuple h-6 w-1.5 right-2 absolute"></div>
-              <div className="bg-pad-lightpuple h-10 w-1.5 right-6 absolute"></div>
-              <div className="bg-pad-lightpuple h-14 w-1.5 right-10 absolute"></div>
-              <div className="bg-pad-lightpuple h-8 w-1.5 right-14 absolute"></div>
-            </div>
-            <p className="text-xl text-pad-beige font-playfair font-bold left-6 top-16 absolute pr-2">
-              Core Values
-            </p>
-            <p className="whitespace-normal text-sm text-pad-beige font-playfair left-6 top-28 absolute pr-2">
-              Bound together by tradition and our common interest in the law, we
-              share these core values
-            </p>
-          </div>
+    <div className="flex flex-col h-52 justify-between bg-pad-darkpurple w-2/5 items-start relative">
+      <div className="h-full w-full border-4 border-pad-lightpurple absolute -left-5 -top-5 z-0" />
+      <div className="flex flex-col h-full justify-between bg-pad-darkpurple w-full items-start relative z-10">
+        <div className="flex h-14 w-full justify-end relative">
+          <div className="h-6 w-1.5 mr-1.5 bg-pad-lightpurple"></div>
+          <div className="h-10 w-1.5 mr-1.5 bg-pad-lightpurple"></div>
+          <div className="h-14 w-1.5 mr-1.5 bg-pad-lightpurple"></div>
+          <div className="h-8 w-1.5 mr-1.5 bg-pad-lightpurple"></div>
+        </div>
+        <div className="m-4 relative">
+          <p className="text-3xl text-pad-beige font-playfair font-bold pr-2">
+            Core Values
+          </p>
+          <p className="whitespace-normal text-sm text-pad-beige font-playfair w-10/12">
+            Bound together by tradition and our common interest in the law, we
+            share these core values
+          </p>
         </div>
       </div>
     </div>
