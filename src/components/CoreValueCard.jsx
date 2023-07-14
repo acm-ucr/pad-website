be793b4f753c0a51696897990d5a6dcaec5eaff7
Menthy <wumengxuan0728@gmail.com>
2023-07-14 10:39:23 -0700

    fix

--- src/components/CoreValueCard.jsx
+++ src/components/CoreValueCard.jsx
@@ -3,9 +3,12 @@ import { FaBlackTie } from "react-icons/fa";
 
 const CoreValueCard = ({ icon, text }) => {
   return (
-    <div className="coreValCard">
-      <FaBlackTie className={icon} />
-      <p className="text"> {text} </p>
+    <div className="bg-pad-purple flex flex-col items-center w-[15%] h-64 justify-center">
+      <FaBlackTie className="text-4xl text-pad-beige" />
+      <div className="h-[2px] w-8 bg-pad-beige my-8" />
+      <p className="text-4xl text-pad-beige font-playfair font-bold">
+        Integrity
+      </p>
     </div>
   );
 };
