0f4650ebe41968f91b65eeb71bfa21d5076a77fb
stormyy00 <jonathan.trujillo0432@gmail.com>
2023-07-19 13:14:22 -0700

    yeah

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,9 +1,12 @@
 import React from "react";
 import Title from "@/components/Title";
 const about = () => {
-  return <div>about
-    <Title/>
-  </div>;
+  return (
+    <div>
+      about
+      <Title />
+    </div>
+  );
 };
 
 export default about;
