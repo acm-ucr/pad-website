c25efc4de085833734cdd10f7e262f184fe06664
stormyy00 <jonathan.trujillo0432@gmail.com>
2023-07-19 13:12:16 -0700

    done

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,7 +1,9 @@
 import React from "react";
-
+import Title from "@/components/Title";
 const about = () => {
-  return <div>about</div>;
+  return <div>about
+    <Title/>
+  </div>;
 };
 
 export default about;
