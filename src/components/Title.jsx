b0c64e3a4aa184ddb071cedee5ba56037452fdd1
stormyy00 <jonathan.trujillo0432@gmail.com>
2023-07-19 12:17:48 -0700

    color adjusted

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -2,15 +2,19 @@ import React from "react";
 
 const Title = () => {
   return (
-  <div className=" h-48 w-full">
-    <div className="relative top-24 ">
-     <div className="text-pad-lightpurple opacity-40 font-bold font-playfair text-7xl relative left-8">Who We Are</div>
-    <div className="text-black font-bold font-playfair text-3xl relative left-20 bottom-7">
-      <span className="underline underline-offset-8 decoration-pad-darkpurple">W</span>
-        <span className="">ho We Are</span>
+    <div className=" h-48 w-full">
+      <div className="relative top-24 ">
+        <div className="text-pad-lightpurple opacity-40 font-bold font-playfair text-7xl relative left-8">
+          Who We Are
+        </div>
+        <div className="text-black font-bold font-playfair text-3xl relative left-20 bottom-7">
+          <span className="underline underline-offset-8 decoration-pad-darkpurple">
+            W
+          </span>
+          <span className="">ho We Are</span>
+        </div>
       </div>
-      </div>
-      </div>
+    </div>
   );
 };
 
