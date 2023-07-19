d9277b37514b2c8b1737d4c801f1b46344a4a634
stormyy00 <jonathan.trujillo0432@gmail.com>
2023-07-19 16:45:37 -0700

    added textshadow

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -4,7 +4,7 @@ const Title = () => {
   return (
     <div className=" h-48 w-full">
       <div className="relative top-24">
-        <div className="text-pad-lightpurple opacity-40 font-bold font-playfair text-7xl relative left-8">
+        <div className="text-pad-textshadow font-bold font-playfair text-7xl relative left-8">
           Who We Are
         </div>
         <div className="text-black font-bold font-playfair text-3xl relative left-20 bottom-7">
