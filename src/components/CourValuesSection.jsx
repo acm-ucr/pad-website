2968c1976fbd7c26bb580d5858ceda99473a9d2b
Jonathan Champeth <jonbrah@JonathansAir98.home>
2023-07-20 18:28:46 -0700

    Added core values

--- src/components/CourValuesSection.jsx
+++ src/components/CourValuesSection.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import CoreValueTitle from "./CoreValueTitle";
+import CoreValues from "./CoreValues";
 
 const CourValuesSection = () => {
   return (
     <div className="flex items-center justify-center m-5">
       <CoreValueTitle />
+      <CoreValues />
     </div>
   );
 };
