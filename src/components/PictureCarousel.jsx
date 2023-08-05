c6ebfc244daeee696b7ce4aa9413ecf412381614
stormyy00 <jonathan.trujillo0432@gmail.com>
2023-08-05 16:53:34 -0700

    updated names

--- src/components/PictureCarousel.jsx
+++ src/components/PictureCarousel.jsx
@@ -18,52 +18,52 @@ const PictureCarousel = () => {
       url: "join.png",
     },
   ];
-  const [currentIndex, setCurrentIndex] = useState(0);
+  const [currentIdx, setCurrentIdx] = useState(0);
 
   const prevSlide = () => {
     const isFirstSlide = currentIndex === 0;
-    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
-    setCurrentIndex(newIndex);
+    const newIdx = isFirstSlide ? slides.length - 1 : currentIdx - 1;
+    setCurrentIdx(newIdx);
   };
 
   const nextSlide = () => {
-    const isLastSlide = currentIndex === slides.length - 1;
-    const newIndex = isLastSlide ? 0 : currentIndex + 1;
-    setCurrentIndex(newIndex);
+    const isLastSlide = currentIdx === slides.length - 1;
+    const newIdx = isLastSlide ? 0 : currentIdx + 1;
+    setCurrentIdx(newIdx);
   };
 
-  const goToSlide = (slideIndex) => {
-    setCurrentIndex(slideIndex);
+  const goToSlide = (slideIdx) => {
+    setCurrentIdx(slideIdx);
   };
 
   return (
     <div className="h-[85vh]">
       <div>
-        <p className="indent-80 font-playfair text-pad-black text-2xl">
-          Phi Alpha Delta Retreat 2022
-        </p>
         <div className="max-w-[800px] h-[480px] w-full m-auto relative group">
+          <p className=" mb-1 font-playfair text-pad-black text-2xl">
+            Phi Alpha Delta Retreat 2022
+          </p>
           <div
-            style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
+            style={{ backgroundImage: `url(${slides[currentIdx].url})` }}
             className="w-full h-full bg-center bg-cover duration-500"
           ></div>
           {/* Left Arrow */}
-          <div className=" absolute top-[50%] -translate-x-36 translate-y-[-50%] left-5  text-pad-purple cursor-pointer">
+          <div className=" absolute top-[50%] -translate-x-52 translate-y-[-50%] left-5  text-pad-purple cursor-pointer">
             <AiOutlineArrowLeft onClick={prevSlide} size={70} />
           </div>
           {/* Right Arrow */}
-          <div className="absolute top-[50%] translate-x-36 translate-y-[-50%] right-5  text-pad-purple cursor-pointer">
+          <div className="absolute top-[50%] translate-x-52 translate-y-[-50%] right-5  text-pad-purple cursor-pointer">
             <AiOutlineArrowRight onClick={nextSlide} size={70} />
           </div>
           <div className="flex top-4 justify-center py-2">
-            {slides.map((slide, slideIndex) => (
+            {slides.map((slide, slideIdx) => (
               <div
-                key={slideIndex}
-                onClick={() => goToSlide(slideIndex)}
+                key={slideIdx}
+                onClick={() => goToSlide(slideIdx)}
                 className={
-                  currentIndex === slideIndex
+                  currentIdx === slideIdx
                     ? "text-4xl cursor-pointer text-white"
-                    : "text-pad-purple cursor-pointer text-4xl"
+                    : "text-pad-lightpurple cursor-pointer text-4xl"
                 }
               >
                 <RxDotFilled />
