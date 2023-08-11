aaa60184503d9e2396225dd685960fa2f89a7b6d
Sreeja Pagireddy <spagi@ucr.edu>
2023-08-11 09:34:37 -0700

    mobile for who

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 const Title = ({ title }) => {
   return (
-    <div className=" h-48 w-full">
+    <div className="md:flex-row h-48 w-full md:items-end md:justify-end">
       <div className="relative top-24">
-        <div className="text-pad-textshadow font-bold font-playfair text-7xl relative left-8">
+        <div className="text-pad-textshadow md:text-m md:items-end font-bold font-playfair text-7xl relative left-8">
           {title}
         </div>
-        <div className="text-black font-bold font-playfair text-3xl relative left-20 bottom-7">
+        <div className="text-black font-bold font-playfair md:text-m text-3xl relative left-20 bottom-7">
           <span className="underline underline-offset-8 decoration-pad-darkpurple">
             {title}
           </span>
