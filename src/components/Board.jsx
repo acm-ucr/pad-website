338c732e4fdfedff6cb95af4bd34e169aec17247
menthy-wu <wumengxuan0728@gmail.com>
2023-08-18 10:21:41 -0700

    fix board image

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,38 +1,40 @@
 "use client";
 import React from "react";
 import { Row, Col } from "react-bootstrap";
+import BoardPlaceHolder from "../../public/boardPlaceholder.png";
+import Image from "next/image";
 
 const Board = () => {
   const boardMembers = [
     {
       position: "Position Title P",
       name: "Firstname Lastname ",
-      image: "./BoardPlaceholder.png",
+      image: BoardPlaceHolder,
     },
     {
       position: "Position Title VP",
       name: "Firstname Lastname 2",
-      image: "./BoardPlaceholder.png",
+      image: BoardPlaceHolder,
     },
     {
       position: "Position Title T",
       name: "Firstname Lastname 3",
-      image: "./BoardPlaceholder.png",
+      image: BoardPlaceHolder,
     },
     {
       position: "Position Title S",
       name: "Firstname Lastname 4",
-      image: "./BoardPlaceholder.png",
+      image: BoardPlaceHolder,
     },
     {
       position: "Position Title SM",
       name: "Firstname Lastname 5",
-      image: "./BoardPlaceholder.png",
+      image: BoardPlaceHolder,
     },
     {
       position: "Position Title A",
       name: "Firstname Lastname 6",
-      image: "./BoardPlaceholder.png",
+      image: BoardPlaceHolder,
     },
   ];
 
@@ -46,7 +48,7 @@ const Board = () => {
           className="p-4 flex items-center justify-center "
         >
           <div className="relative">
-            <img src={member.image} alt={`Board Member ${idx + 1}`} />
+            <Image src={member.image} alt={`Board Member ${idx + 1}`} />
             <div className="absolute -bottom-2 -left-2">
               <div className="bg-pad-purple w-full h-8 shadow">
                 <p className="text-pad-beige font-bold font-playfair p-1">
