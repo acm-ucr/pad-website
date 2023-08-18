90973fabcbf135cf8f1b93b819c4ac16cf2542e5
menthy-wu <wumengxuan0728@gmail.com>
2023-08-18 01:37:00 -0700

    bootstrap grid

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,4 +1,6 @@
+"use client";
 import React from "react";
+import { Row, Col } from "react-bootstrap";
 
 const Board = () => {
   const boardMembers = [
@@ -35,47 +37,46 @@ const Board = () => {
   ];
 
   return (
-    <div className="container">
-      <div className="h-full w-full mt-6 mb-6">
-        <div className="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-4">
-          {boardMembers.map((member, idx) => (
-            <div className="w-full h-96" key={idx}>
-              <img
-                src={member.image}
-                className="relative top-6 left-16"
-                alt={`Board Member ${idx + 1}`}
-              />
-              <div className="relative -top-1 left-16">
-                <div className="bg-pad-purple w-48 h-8 shadow">
-                  <p className="text-pad-beige font-bold font-playfair p-1">
-                    {member.position}
-                  </p>
-                  <div
-                    className="w-0 h-0 =
+    <Row className=" my-6 p-0 flex justify-start w-10/12">
+      {boardMembers.map((member, idx) => (
+        <Col
+          sm={6}
+          md={3}
+          key={idx}
+          className="p-4 flex items-center justify-center "
+        >
+          <div className="relative">
+            <img src={member.image} alt={`Board Member ${idx + 1}`} />
+            <div className="absolute -bottom-2 -left-2">
+              <div className="bg-pad-purple w-full h-8 shadow">
+                <p className="text-pad-beige font-bold font-playfair p-1">
+                  {member.position}
+                </p>
+                <div
+                  className="w-0 h-0 =
                 border-l-[0px] border-l-transparent
                 border-t-[32.5px] border-t-pad-purple
                 border-r-[40px] border-r-transparent
                 relative bottom-12 left-48"
-                  ></div>
-                </div>
-                <div className="bg-pad-lightpurple w-48 h-8 shadow">
-                  <p className="text-pad-purple font-bold font-playfair p-1">
-                    {member.name}
-                  </p>
-                  <div
-                    className="w-0 h-0 
+                ></div>
+              </div>
+              <div className="bg-pad-lightpurple w-48 h-8 shadow">
+                <p className="text-pad-purple font-bold font-playfair p-1">
+                  {member.name}
+                </p>
+                <div
+                  className="w-0 h-0 
                 border-l-[0px] border-l-transparent
                 border-b-[32.5px] border-b-pad-lightpurple
                 border-r-[40px] border-r-transparent
                 relative bottom-[48.5px] left-48"
-                  ></div>
-                </div>
+                ></div>
               </div>
             </div>
-          ))}
-        </div>
-      </div>
-    </div>
+          </div>
+        </Col>
+      ))}
+    </Row>
   );
 };
 
