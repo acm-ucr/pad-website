2e797a0fba0aaabe17193acfe3d2814e3d5ebafb
Sreeja Pagireddy <spagi@ucr.edu>
2023-08-24 13:22:08 -0700

    404 page

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,7 +1,20 @@
 import React from "react";
 
 const notFound = () => {
-  return <div>404</div>;
+  return (
+    <div className="flex h-screen">
+      <div className="m-auto">
+        <h1 className="text-7xl font-readex text-pad-lightpurple">404</h1>
+
+        <p className="text-3xl font-readex -indent-5 text-code-black">
+          Page Not Found
+        </p>
+        <div className="border-2 border-pad-black relative right-4 text-center">
+          Back To Home
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default notFound;
