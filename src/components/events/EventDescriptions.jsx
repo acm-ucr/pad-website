a8905928d6e736622dfa535be079eff6082b8797
stormyy00 <jonathan.trujillo0432@gmail.com>
2023-09-09 01:28:37 -0700

    added animation

--- src/components/events/EventDescriptions.jsx
+++ src/components/events/EventDescriptions.jsx
@@ -7,7 +7,16 @@ const EventDescriptions = ({ events }) => {
       <Title title="Upcoming Events" />
       <div className="self-center w-9/12">
         {events.map((event, index) => {
-          return <EventDescription key={index} event={event} />;
+          return (
+            <EventDescription
+              key={index}
+              event={event}
+              className={`${
+                inView &&
+                `animate-fade-up animate-once animate-delay-[${index}00ms]`
+              }`}
+            />
+          );
         })}
       </div>
     </div>
