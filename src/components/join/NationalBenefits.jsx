966b89ce297db78d21fded6a55fb5bd426893df6
menthy-wu <wumengxuan0728@gmail.com>
2023-09-26 14:42:31 -0700

    join spacing

--- src/components/join/NationalBenefits.jsx
+++ src/components/join/NationalBenefits.jsx
@@ -1,44 +1,30 @@
 const NationalBenefits = () => {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col gap-2">
       <div className="text-pad-purple font-bold font-playfair text-2xl border-b-4 border-pad-lightpurple w-fit">
         NATIONALS
       </div>
-      <ul className="mt-3">
-        <li className="indent-0 text-black font-playfair text-sm md:text-lg list-disc">
-          National Directory
-          <ol>
-            <li className="list-disc text-black font-playfair text-sm md:text-lg ">
-              List of every Phi Alpha Delta
-              <p>member</p>
-            </li>
-          </ol>
-        </li>
-      </ul>
-      <ul className="-mt-4">
-        <li className="text-black font-playfair text-sm md:text-lg list-disc">
-          Exclusive discounts through
-          <div>partners</div>
-          <ol>
-            <li className=" list-disc text-black font-playfair text-sm md:text-lg">
-              Office Depot, Test Prep
-              <p>Courses, etc.</p>
-            </li>
-          </ol>
-        </li>
-      </ul>
-      <ul className="-mt-4 list-disc">
-        <li className="text-black font-playfair text-sm md:text-lg">
-          Scholarships
-        </li>
-        <li className="text-black font-playfair text-sm md:text-lg">
-          Internships
-        </li>
-        <li className="text-black font-playfair text-sm md:text-lg">
-          Law School Transition
-          <p>Resources</p>
-        </li>
-      </ul>
+      <p className="pl-0 m-0 text-black font-playfair text-sm md:text-lg">
+        • National Directory
+      </p>
+      <p className="pl-10 m-0 text-black font-playfair text-sm md:text-lg">
+        • List of every Phi Alpha Delta member
+      </p>
+      <p className="pl-0 m-0 text-black font-playfair text-sm md:text-lg">
+        • Exclusive discounts through partners
+      </p>
+      <p className="pl-10 m-0 text-black font-playfair text-sm md:text-lg">
+        • Office Depot, Test Prep Courses, etc.
+      </p>
+      <p className="pl-0 m-0 text-black font-playfair text-sm md:text-lg">
+        • Scholarships
+      </p>
+      <p className="pl-0 m-0 text-black font-playfair text-sm md:text-lg">
+        • Internships
+      </p>
+      <p className="pl-0 m-0 text-black font-playfair text-sm md:text-lg">
+        • Law School Transition Resources
+      </p>
     </div>
   );
 };
--- src/components/join/UCRBenefits.jsx
+++ src/components/join/UCRBenefits.jsx
@@ -4,46 +4,36 @@ const UCRBenefits = () => {
       <div className="text-pad-purple font-bold font-playfair text-2xl border-b-4 border-pad-lightpurple w-fit">
         UCR CHAPTER
       </div>
-      <ul className="mt-3">
-        <li className="indent-0 text-black font-playfair text-sm md:text-lg list-disc">
-          PartnerShips
-          <ol>
-            <li className="list-disc text-black font-playfair text-sm md:text-lg whitespace-pre-line">
-              ScoreitUp. Mark Sacks, Apollo
-            </li>
-          </ol>
-        </li>
-      </ul>
-      <ul className="-mt-4">
-        <li className="text-black font-playfair text-sm md:text-lg list-disc">
-          Workshops
-          <ol>
-            <li className=" list-disc text-black font-playfair text-sm md:text-lg">
-              How to get into Law School
-            </li>
-            <li className=" list-disc text-black font-playfair text-sm md:text-lg">
-              Personal Statments
-            </li>
-            <li className=" list-disc text-black font-playfair text-sm md:text-lg">
-              Resume Building
-            </li>
-            <li className="list-disc text-black font-playfair text-sm md:text-lg">
-              Access to helpful Alumni
-            </li>
-          </ol>
-        </li>
-      </ul>
-      <ul className="-mt-4 list-disc">
-        <li className="text-black font-playfair text-sm md:text-lg">
-          LSAT Study Groups
-        </li>
-        <li className="text-black font-playfair text-sm md:text-lg">
-          Mentor/Mentee Program
-        </li>
-        <li className="text-black font-playfair text-sm md:text-lg">
-          Lawyer Banquet
-        </li>
-      </ul>
+      <p className="pl-0 m-0 text-black font-playfair text-sm md:text-lg">
+        • PartnerShips
+      </p>
+      <p className="pl-10 m-0 text-black font-playfair text-sm md:text-lg">
+        • ScoreitUp. Mark Sacks, Apollo
+      </p>
+      <p className="pl-0 m-0 text-black font-playfair text-sm md:text-lg">
+        • Workshops
+      </p>
+      <p className="pl-10 m-0 text-black font-playfair text-sm md:text-lg">
+        • How to get into Law School
+      </p>
+      <p className="pl-10 m-0 text-black font-playfair text-sm md:text-lg">
+        • Personal Statments
+      </p>
+      <p className="pl-10 m-0 text-black font-playfair text-sm md:text-lg">
+        • Resume Building
+      </p>
+      <p className="pl-10 m-0 text-black font-playfair text-sm md:text-lg">
+        • Access to helpful Alumni
+      </p>
+      <p className="pl-0 m-0 text-black font-playfair text-sm md:text-lg">
+        • LSAT Study Groups
+      </p>
+      <p className="pl-0 m-0 text-black font-playfair text-sm md:text-lg">
+        • Mentor/Mentee Program
+      </p>
+      <p className="pl-0 m-0 text-black font-playfair text-sm md:text-lg">
+        • Lawyer Banquet
+      </p>
     </div>
   );
 };
