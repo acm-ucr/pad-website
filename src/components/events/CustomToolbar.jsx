7c2198be58713eb68dba7cc4b78c397ba226908e
menthy-wu <wumengxuan0728@gmail.com>
2023-10-01 16:11:00 -0700

    fix calendar toolbar

--- src/components/events/CustomToolbar.jsx
+++ src/components/events/CustomToolbar.jsx
@@ -1,6 +1,7 @@
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import filters from "@/data/filters";
+import { AiOutlineArrowLeft, AiOutlineArrowRight } from "react-icons/ai";
 const monthNames = [
   "January",
   "February",
@@ -18,9 +19,20 @@ const monthNames = [
 const CustomToolbar = (event) => {
   return (
     <div className="flex justify-center items-center w-full py-3 flex-col md:flex-row">
-      <div className="md:w-1/3 flex gap-3 mb-2 md:mb-0">
+      <div className="md:w-1/3 flex gap-3 mb-2 md:mb-0 items-center">
+        <AiOutlineArrowLeft
+          onClick={() => {
+            console.log("hi");
+            event.onNavigate("PREV");
+          }}
+          className="hover:cursor-pointer"
+        />
         <p className="m-0">{monthNames[event.date.getMonth()]}</p>
         <p className="m-0">{event.date.getFullYear()}</p>
+        <AiOutlineArrowRight
+          onClick={() => event.onNavigate("NEXT")}
+          className="hover:cursor-pointer"
+        />
       </div>
       <Row className="w-10/12 md:w-3/4 m-0 p-0 flex justify-center md:justify-end justify-items-center items-center">
         {filters.map((filter, index) => (
